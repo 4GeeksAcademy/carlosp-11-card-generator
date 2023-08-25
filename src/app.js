78affda449e1ad64ef6ec675f9c01f44eb4736a9
Carlos Daniel Ponce <135666468+carlosp-11@users.noreply.github.com>
2023-08-25 23:28:38 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +6,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ["♦", "♥", "♠", "♣"];
+  let valor = Math.floor(Math.random() * 14);
+  let aux = Math.floor(Math.random() * palo.length);
+  let paloC = palo[aux];
+  if (valor <= 1) valor = "A";
+  if (valor == 11) valor = "J";
+  if (valor == 12) valor = "Q";
+  if (valor == 13) valor = "K";
+  if (aux <= 1) {
+    let myColor = document.querySelectorAll(".fontSuit");
+    Array.from(myColor).forEach(function(item) {
+      item.classList.add("redSuit");
+    });
+  }
+
+  let myCard = document.querySelector(".fontOne");
+  myCard.innerHTML = valor;
+
+  let mySuit = document.querySelectorAll(".fontSuit");
+  Array.from(mySuit).forEach(function(elem) {
+    elem.innerHTML = paloC;
+  });
 };
