f139b6d5050cf2c4a4bd9fcb488ae72131a54ba9
Carlos Daniel Ponce <135666468+carlosp-11@users.noreply.github.com>
2023-08-26 12:54:10 +0000

    mejorando codigo JS

--- src/app.js
+++ src/app.js
@@ -4,28 +4,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//Función para generar un número entero aleatorio con valor máximo no incluido
+const numeroAlAzar = function(valorMax) {
+  return Math.floor(Math.random() * valorMax);
+};
+
+//Variable global con array de elementos que pertenecen a una clase
+let mySuit = document.querySelectorAll(".fontSuit");
+let myNumber = document.querySelector(".fontOne");
+
 window.onload = function() {
   //write your code here
-  let palo = ["♦", "♥", "♠", "♣"];
-  let valor = Math.floor(Math.random() * 14);
-  let aux = Math.floor(Math.random() * palo.length);
-  let paloC = palo[aux];
-  if (valor <= 1) valor = "A";
-  if (valor == 11) valor = "J";
-  if (valor == 12) valor = "Q";
-  if (valor == 13) valor = "K";
-  if (aux <= 1) {
-    let myColor = document.querySelectorAll(".fontSuit");
-    Array.from(myColor).forEach(function(item) {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numeroAzar = numeros[numeroAlAzar(numeros.length)];
+  let paloAzar = palos[numeroAlAzar(palos.length)];
+  if (paloAzar == "♦" || paloAzar == "♥") {
+    mySuit.forEach(function(item) {
       item.classList.add("redSuit");
     });
   }
+  if (numeroAzar == "10") {
+    myNumber.classList.add("newMargin");
+  }
 
-  let myCard = document.querySelector(".fontOne");
-  myCard.innerHTML = valor;
+  myNumber.innerHTML = numeroAzar;
 
-  let mySuit = document.querySelectorAll(".fontSuit");
-  Array.from(mySuit).forEach(function(elem) {
-    elem.innerHTML = paloC;
+  mySuit.forEach(function(elem) {
+    elem.innerHTML = paloAzar;
   });
 };
